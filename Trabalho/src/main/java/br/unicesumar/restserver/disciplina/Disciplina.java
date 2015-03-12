5f6d1e7e5536a6f4934424c6708fd9211159e520
fsaguilieri <francielloa@ig.com.br>
2015-03-12 20:38:39 -0300

    Adicao da branch questao#02

---
+++ Trabalho/src/main/java/br/unicesumar/restserver/disciplina/Disciplina.java
@@ -0,0 +1,49 @@
+package br.unicesumar.restserver.disciplina;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Disciplina {
+    
+    @Id
+    private Long id;
+    private String nome;
+    private Integer cargaHoraria;
+    private Double peso;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(Integer cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+    
+    
+    
+}
---
+++ Trabalho/src/main/java/br/unicesumar/restserver/disciplina/DisciplinaController.java
@@ -0,0 +1,35 @@
+package br.unicesumar.restserver.disciplina;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/disciplina")
+@Transactional
+public class DisciplinaController {
+
+    @Autowired
+    private EntityManager em;
+
+
+    public List<Disciplina> getDisciplina() {
+        return null;
+    }
+    
+    public void criarDisciplina(@RequestBody Disciplina disciplina) {
+        
+    }        
+    
+    public void alterarDisciplina(@RequestBody Disciplina disciplina) {
+    }        
+    
+    public void excluirDisciplina(@PathVariable Long id) {
+    }        
+    
+}

15722e5e0cc7f4a81fbb5c4eb8f23cd6b0958627
fsaguilieri <francielloa@ig.com.br>
2015-03-12 20:49:21 -0300

    Adicao do front

---
+++ Trabalho/src/main/webapp/pages/disciplina/disciplina.js
@@ -0,0 +1,48 @@
+module = angular.module("Prova", []);
+
+module.controller("DisciplinaController", ["$scope","$http", DisciplinaController]);
+
+
+function DisciplinaController($scope,$http) {
+    
+    $scope.iniciar = funcaoIniciar;
+    $scope.salvar = funcaoSalvar;
+    $scope.excluir = funcaoExcluir;
+    $scope.editar = funcaoEditar;
+    
+    $scope.disciplinas = [];
+    $scope.disciplina = {};
+    $scope.isNovo = true;
+    
+    function funcaoEditar(disciplina) {
+        $scope.disciplina = angular.copy(disciplina);
+        $scope.isNovo = false;
+    }
+
+    
+    function funcaoExcluir(disciplina) {
+    }
+    
+    function funcaoSalvar() {
+    }
+    
+    function funcaoCarregar() {
+        $http.get("/disciplinas").success(onSuccess).error(onError);
+        
+        function onSuccess(data, status) {
+            $scope.disciplinas = data;       
+            console.log(data);
+        }
+        function onError(data, status) {
+            alert("Deu erro: " + data);
+        }
+    }
+    
+    function funcaoIniciar() {
+        funcaoCarregar();
+        console.log(">>> disciplinas carregadas....");
+    }
+        
+}
+
+

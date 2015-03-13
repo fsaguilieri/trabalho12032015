4d689b68a10b1bcb9ecf02e8ae9212d0ae48ba96
fsaguilieri <francielloa@ig.com.br>
2015-03-12 21:14:46 -0300

    Alteracao do back

--- Trabalho/src/main/java/br/unicesumar/restserver/disciplina/DisciplinaController.java
+++ Trabalho/src/main/java/br/unicesumar/restserver/disciplina/DisciplinaController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,13 +23,16 @@ public class DisciplinaController {
         return null;
     }
     
+    @RequestMapping(method = RequestMethod.POST)
     public void criarDisciplina(@RequestBody Disciplina disciplina) {
         
     }        
     
+    @RequestMapping(method = RequestMethod.PUT)
     public void alterarDisciplina(@RequestBody Disciplina disciplina) {
     }        
     
+    @RequestMapping(method = RequestMethod.DELETE)
     public void excluirDisciplina(@PathVariable Long id) {
     }        
     
